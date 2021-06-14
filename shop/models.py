625c3ee2fffba4777f6d19ae9393303a1f44a31b
AlexBulavin <bulavin1970@icloud.com>
2021-06-14 14:01:23 +0300

    Add cart methods. Some problems during migration need to be fix.

--- shop/models.py
+++ shop/models.py
@@ -60,7 +60,7 @@ class DeliveryType(models.Model):
     def __str__(self): #Для отображения в БД реальных названий полей
         return self.delivery_name
 
-class CatItem(models.Model):
+class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
